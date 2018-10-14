93763911f15dbd40f5d86942cd5225d1a0684633
Eric Laitman <ericlaitman@Erics-MacBook-Pro-2.local>
2018-10-14 00:33:02 -0400

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,87 @@
+class Author
+
+  attr_accessor :name
+  @@all = []
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+
+  def add_post(post)
+    post.author = self
+  end
+  def add_post_by_title(title)
+    post.author = Post.new(title)
+    post.author = self
+  end
+
+  def self.post_count
+    Post.all.count
+  end
+
+    def posts
+     Post.all.select {|post| post.author == self}
+     end
+
+     def add_post_by_title(title)
+      post = Post.new(title)
+      post.author = self
+     end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+#
+# class Author
+#   attr_accessor :name
+#
+#   @@all = []
+#
+#   def initialize(name)
+#     @name = name
+#     @@all << self
+#   end
+#
+#   def self.all
+#     @@all
+#   end
+#
+#   def add_post(post)
+#     post.author = self
+#   end
+#
+
+#
+
+# end
